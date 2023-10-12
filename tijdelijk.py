3f38816d31946803fcd83c4cc1e561619895708e
monjed9516 <monikajedrzejak85@gmail.com>
2023-10-12 17:39:43 +0100

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,17 @@
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+aanbieding = prijzen["vanille"] * 0.8
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €  {aanbieding}"
+reclame_tekst2 = reclame_tekst.upper()
+reclame_tekst3 = reclame_tekst2.split()
+print(reclame_tekst3)
+for el in reclame_tekst3:
+    if len(el) >= 5:
+        print(el)
+    else:
+        print(el.lower())
+
+
